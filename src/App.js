376f123b05a6ead2dd983baae4f1b91b1e68417b
mariamihai <maria.ic.mihai@gmail.com>
2020-12-14 15:23:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App.</h1>
+        <p>I am a paragraph.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
