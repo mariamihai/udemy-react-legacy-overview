1b4d5047fe64e801d9efb09ffc24da40d0334c8c
mariamihai <maria.ic.mihai@gmail.com>
2020-12-14 17:21:52 +0100

    [Section 3.34-37] Adding dynamic content for components

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p>I am a paragraph.</p>
-        <Person />
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
   }
