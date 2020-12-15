5a4306a9e1967c4ff695646f9272d1b52886f331
mariamihai <maria.ic.mihai@gmail.com>
2020-12-15 11:44:26 +0100

    [Section 3.42] Manipulating state. Added handler for the button.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ class App extends Component {
       {name : "Max", age : 28},
       {name : "Manu", age : 29},
       {name : "Stephanie", age : 26}
-    ]
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log("calling all buttons in the state!");
+    // NO! => this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        {name : "Maximilian", age : 28},
+        {name : "Manu", age : 29},
+        {name : "Stephanie", age : 27}
+      ]
+    })
   }
 
   render() {
@@ -16,8 +29,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <p>I am a paragraph.</p>
-
-        <button>Switch name</button>
+ 
+        <button onClick={this.switchNameHandler}>Switch name</button>
 
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: racing</Person>
