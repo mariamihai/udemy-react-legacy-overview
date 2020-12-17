44d2c5b46305b2312757683b9db78bbac84db256
mariamihai <maria.ic.mihai@gmail.com>
2020-12-17 21:24:10 +0100

    [Section 5.73] Adding styled components & dynamic styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? '#ffcccb' : 'lightgreen'};
+    color: black
+  }
+`
 
 class App extends Component {
   state = {
@@ -41,19 +56,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
 
     let persons = null;
     if(this.state.showPersons) {
-      style.backgroundColor = 'red';
-
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
@@ -83,9 +88,10 @@ class App extends Component {
         <h1>Hi, I'm a React App.</h1>
         <p className={paragraphClasses.join( ' ' )}>I am a paragraph.</p>
 
-        <button onClick={this.togglePersonsHandler} style={style}>
+        <StyledButton alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}>
           Toggle persons
-        </button>
+        </StyledButton>
 
         {persons}
       </div>
