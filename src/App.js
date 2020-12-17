d28dc251b122ede3bdafa495b3606abe456a9522
mariamihai <maria.ic.mihai@gmail.com>
2020-12-17 11:46:38 +0100

    [Section 4.60] Flexible lists - change person name based on input dynamically

--- src/App.js
+++ src/App.js
@@ -13,14 +13,17 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        {name : 'Max', age : 28},
-        {name : event.target.value, age : 29},
-        {name : 'Stephanie', age : 26}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => id === p.id);
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = { ...this.state.persons[personIndex] }
+    person.name = event.target.value;
+
+    const persons = [ ...this.state.persons ];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
   
   deletePersonsHandler = (personIndex) => {
@@ -53,6 +56,7 @@ class App extends Component {
           {this.state.persons.map((person, index) => {
             return <Person 
               click={() => this.deletePersonsHandler(index)}
+              change={(event) => this.nameChangedHandler(event, person.id)}
               name={person.name} 
               age={person.age}
               key={person.id}/>
