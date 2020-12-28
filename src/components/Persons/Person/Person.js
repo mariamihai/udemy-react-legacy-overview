55069421c637c08da44c6ede344aff568a8a79ca
mariamihai <maria.ic.mihai@gmail.com>
2020-12-28 15:12:26 +0100

    [Section 7. 107] Removed unused import in Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
