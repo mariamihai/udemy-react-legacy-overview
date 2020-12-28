97fd7757ac6565631adc0aa51f57bfc6ba248aa9
mariamihai <maria.ic.mihai@gmail.com>
2020-12-28 13:12:30 +0100

    [Section 7.102] Rendering Adjacent JSX Elements - by returning an array or with higher order component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Auxiliary from '../../../hoc/Auxiliary';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering');
 
+        // return (
+        //     <div className={classes.Person}>
+        //         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+        //         <p>{this.props.children}</p>
+        //         <input type="text" onChange={this.props.change}
+        //             value={this.props.name} />
+        //     </div>
+        // );
+
+        // // Return an array
+        // return [
+        //     <p key='k1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+        //     <p key='k2'>{this.props.children}</p>,
+        //     <input key='k3' type="text" onChange={this.props.change}
+        //         value={this.props.name} />
+        // ];
+
+        // Use a higher order component
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.change}
+            <Auxiliary>
+                <p key='k1' onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p key='k2'>{this.props.children}</p>
+                <input key='k3' type="text" onChange={this.props.change}
                     value={this.props.name} />
-            </div>
+            </Auxiliary>
         );
     }
 }
