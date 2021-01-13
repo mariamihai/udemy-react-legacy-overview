f3e6aa4db8bce553fd40a986ce3f58eb3cdedf07
mariamihai <maria.ic.mihai@gmail.com>
2021-01-13 17:43:39 +0100

    [Section 11. 198] Removed the "withRouter" HOC from Post.js

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 
 import styles from './Post.module.css';
 
-const post = (props) => {
+const post = (props) => (
+    <article className={styles.Post}
+        onClick={props.click}>
+        <h1>{props.title}</h1>
 
-    // Can use history, location and match here
+        <div className={styles.Info}>
+            <div className={styles.Author}>{props.author}</div>
+        </div>
+    </article>
+);
 
-    return (
-        <article className={styles.Post}
-            onClick={props.click}>
-            <h1>{props.title}</h1>
-
-            <div className={styles.Info}>
-                <div className={styles.Author}>{props.author}</div>
-            </div>
-        </article>
-    );
-};
-
-export default withRouter(post);
+export default post;
