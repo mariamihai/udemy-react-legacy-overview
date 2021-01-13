b16e75fe833b9c3119072555bb416671687cc470
mariamihai <maria.ic.mihai@gmail.com>
2021-01-13 13:58:13 +0100

    Clean up axios.js

--- src/axios.js
+++ src/axios.js
@@ -4,17 +4,12 @@ const instance = axios.create({
     baseURL: 'http://jsonplaceholder.typicode.com'
 });
 
-// Alter defaults after instance has been created
 instance.defaults.headers.common['Authorization'] = 'AUTH_TOKEN_IN_INSTANCE';
 
-var requestInterceptor = instance.interceptors.request.use(config => {
-    console.log(config);
+instance.interceptors.request.use(config => {
     return config;
   }, error => {
-    console.log(error);
     return Promise.reject(error);
   });
   
-// axios.interceptors.request.eject(requestInterceptor);
-
 export default instance;
