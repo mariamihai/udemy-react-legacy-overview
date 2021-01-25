3bd5fe86363dacb1b6fc4e36399983588457a8a6
mariamihai <maria.ic.mihai@gmail.com>
2021-01-25 16:13:01 +0100

    Updated with instructor's solution.

--- src/App.js
+++ src/App.js
@@ -5,31 +5,34 @@ import { BrowserRouter, NavLink, Route, Switch, Redirect } from 'react-router-do
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
 
+import NoMatch from './components/NoMatch/NoMatch';
+
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to='/users' exact>
-              Users
-        </NavLink>
-          </li>
-
-          <li>
-            <NavLink to='/courses'>
-              Courses
-        </NavLink>
-          </li>
-        </ul>
-      </nav>
 
       <div className="App">
+        <nav>
+          <ul style={{ listStyle: 'none', margin: 'auto', padding: '0' }}>
+            <li style={{ margin: '10px', display: 'inline-block' }}>
+              <NavLink to='/users' exact>
+                Users
+        </NavLink>
+            </li>
+
+            <li style={{ margin: '10px', display: 'inline-block' }}>
+              <NavLink to='/courses'>
+                Courses
+        </NavLink>
+            </li>
+          </ul>
+        </nav>
+
         <Switch>
           <Route path="/users" component={Users} />
           <Route path="/courses" component={Courses} />
           <Redirect from="/all-courses" to="/courses" />
-          <Route render={() => <h1>Page not found</h1>} />
+          <Route component={NoMatch} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render() {
+
+    state = {
+        title: '_COURSE_TITLE_'
+    }
+
+    componentDidMount() {
+        this.setTitle();
+    }
+
+    componentDidUpdate() {
+        this.setTitle();
+    }
+
+    setTitle = () => {
         const query = new URLSearchParams(this.props.location.search);
-        let title = '_COURSE_TITLE_';
 
         for (let param of query.entries()) {
-            if (param[0] === 'title') {
-                title = param[1];
+            if (param[0] === 'title' && this.state.title !== param[1]) {
+                this.setState({ title: param[1] });
             }
         }
+    }
+
+    render() {
 
         return (
             <div>
-                <h1>{title}</h1>
+                <h1>{this.state.title}</h1>
                 <p>You selected the Course with ID: {this.props.match.params.courseId}</p>
             </div>
         );
