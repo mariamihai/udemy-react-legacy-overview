13cdc47a20366a398fc8e01afe5bde4a15b7865e
mariamihai <maria.ic.mihai@gmail.com>
2021-01-26 15:13:56 +0100

    [Section 12.225] Passing the ingredients via query params

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,7 +101,15 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.props.history.push("/checkout");
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryParams.join('&')
+        });
         // this.setState({ loading: true });
 
         // const order = {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -20,10 +20,21 @@ class Checkout extends React.Component {
         this.props.history.replace("/checkout/contact-data");
     }
 
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        }
+        
+        this.setState({ingredients: ingredients});
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary 
+                <CheckoutSummary
                     ingredients={this.state.ingredients}
                     checkoutCancel={this.cancelOrderHandler}
                     checkoutContinue={this.continueOrderHandler}/>
