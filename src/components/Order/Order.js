544f16e50c701e620d6784b20a02637d9fc204bf
mariamihai <maria.ic.mihai@gmail.com>
2021-01-27 15:57:03 +0100

    [Section 12.230-231] Fetching & outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 import styles from './Order.module.css';
 
-const order = (props) => (
-    <div className={styles.Order}>
-        <p>Ingredients: Salad(1)</p>
-        <p>Total price: <strong>USD 5.45</strong></p>
-    </div>
-);
+const order = (props) => {
+    const ingredients = [];
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+    const intredientOutput = ingredients.map(ingredient => {
+        return <span
+            className={styles.OrderIngredient}
+            key={ingredient.name}>
+            {ingredient.name} ({ingredient.amount}) </span>
+    });
+
+    return (
+        <div className={styles.Order}>
+            <p>Ingredients: {intredientOutput}</p>
+            <p>Total price: <strong>USD {props.totalPrice.toFixed(2)}</strong></p>
+        </div>
+    )
+};
 
 export default order;
